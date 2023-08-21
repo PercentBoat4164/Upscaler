4f1017bd8ee4a5d70ec921d55bb919d391142c0d
PercentBoat4164 <thadd.schelp@gmail.com>
2023-08-21 14:20:24 -0400

    Got Unity talking to C++.

--- Assets/DLSSPluginScript.cs
+++ Assets/DLSSPluginScript.cs
@@ -4,17 +4,20 @@ using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-[PluginAttr("DLSSPlugin")]  // Plugin must be stored in "Assets/Plugins".
+// [PluginAttr("DLSSPlugin")]  // Plugin must be stored in "Assets/Plugins".
 public class NewBehaviourScript : MonoBehaviour
 {
     public delegate void debugCallback(IntPtr message);
-    [PluginFunctionAttr("SetDebugCallback")]  // Must match the name of the function in the C++ file.
-    public static _SetDebugCallback SetDebugCallback = null;  // The first identifier does not matter. The second is the name that will be used in the C# script.
-    public delegate void _SetDebugCallback(debugCallback cb);  // Signature must match that of the C++ function.
+//     [PluginFunctionAttr("SetDebugCallback")]  // Must match the name of the function in the C++ file.
+//     public static _SetDebugCallback SetDebugCallback = null;  // The first identifier does not matter. The second is the name that will be used in the C# script.
+//     public delegate void _SetDebugCallback(debugCallback cb);  // Signature must match that of the C++ function.
+//
+//     [PluginFunctionAttr("InitializeNGX")]
+//     public static _InitializeNGX InitializeNGX = null;
+//     public delegate void _InitializeNGX();
 
-    [PluginFunctionAttr("InitializeNGX")]
-    public static _InitializeNGX InitializeNGX = null;
-    public delegate void _InitializeNGX();
+    [DllImport("DLSSPlugin")] public static extern void SetDebugCallback(debugCallback cb);
+    [DllImport("DLSSPlugin")] public static extern void InitializeNGX();
 
     [MonoPInvokeCallback(typeof(debugCallback))]
     void LogDebugMessage(IntPtr message) {
--- Source/main.cpp
+++ Source/main.cpp
@@ -9,19 +9,18 @@
 namespace Logger {
 void (*Info)(const char *) = nullptr;
 
-void log(std::string t_message) {
-  Info(t_message.c_str());
-}
+void log(std::string t_message) { Info(t_message.c_str()); }
 
 void log(std::string t_actionDescription, NVSDK_NGX_Result t_result) {
   switch (t_result) {
-      case NVSDK_NGX_Result_Success:
-        log(t_actionDescription + ": Success"); break;
-      default:
-        log(t_actionDescription + ": Fail");
-    }
+  case NVSDK_NGX_Result_Success:
+    log(t_actionDescription + ": Success");
+    break;
+  default:
+    log(t_actionDescription + ": Fail");
+  }
 }
-}
+} // namespace Logger
 
 namespace Vulkan {
 VkResult (*createInstance)(const VkInstanceCreateInfo *pCreateInfo,
@@ -31,31 +30,39 @@ VkResult (*createInstance)(const VkInstanceCreateInfo *pCreateInfo,
 
 namespace Unity {
 bool interceptedGraphicsInitialization;
-IUnityGraphicsVulkan* vulkanGraphicsInterface;
-}
+IUnityGraphicsVulkan *vulkanGraphicsInterface;
+} // namespace Unity
 
 extern "C" UNITY_INTERFACE_EXPORT void InitializeNGX() {
-  NVSDK_NGX_Result result = NVSDK_NGX_VULKAN_Init(NVSDK_NGX_ENGINE_TYPE_CUSTOM, nullptr, nullptr, nullptr, nullptr);
+  NVSDK_NGX_Result result = NVSDK_NGX_VULKAN_Init(
+      NVSDK_NGX_ENGINE_TYPE_CUSTOM, nullptr, nullptr, nullptr, nullptr);
   Logger::log("Initialize NGX SDK", result);
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void
 SetDebugCallback(void (*t_debugFunction)(const char *)) {
   Logger::Info = t_debugFunction;
-  if (Unity::interceptedGraphicsInitialization) Logger::log("DLSS Plugin loaded successfully.");
-  else Logger::log("DLSS Plugin failed to load.");
+  if (Unity::interceptedGraphicsInitialization)
+    Logger::log("DLSS Plugin loaded successfully.");
+  else
+    Logger::log("DLSS Plugin failed to load.");
 }
 
 extern "C" UNITY_INTERFACE_EXPORT PFN_vkGetInstanceProcAddr
 interceptInitialization(PFN_vkGetInstanceProcAddr t_instance, void *user_data) {
-  Vulkan::createInstance = (typeof(Vulkan::createInstance))t_instance(nullptr, "vkCreateInstance");
+  Vulkan::createInstance =
+      (typeof(Vulkan::createInstance))t_instance(nullptr, "vkCreateInstance");
   return t_instance;
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
 UnityPluginLoad(IUnityInterfaces *t_unityInterfaces) {
-  Unity::vulkanGraphicsInterface = t_unityInterfaces->Get<IUnityGraphicsVulkan>();
-  Unity::interceptedGraphicsInitialization = Unity::vulkanGraphicsInterface->InterceptInitialization(interceptInitialization, nullptr);
+//  Unity::vulkanGraphicsInterface =
+//      t_unityInterfaces->Get<IUnityGraphicsVulkan>();
+//  Unity::interceptedGraphicsInitialization =
+//      Unity::vulkanGraphicsInterface->InterceptInitialization(
+//          interceptInitialization, nullptr);
+  Unity::interceptedGraphicsInitialization = true;
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload() {
