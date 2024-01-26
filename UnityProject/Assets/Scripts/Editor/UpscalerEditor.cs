384ec781bae854837b2ec361b3d8880c8a64eeb1
PercentBoat4164 <thadd.schelp@gmail.com>
2024-01-26 00:48:22 -0500

    Added tooltips. Started documentation.

--- UnityProject/Assets/Scripts/Editor/UpscalerEditor.cs
+++ UnityProject/Assets/Scripts/Editor/UpscalerEditor.cs
@@ -28,28 +28,48 @@ public class UpscalerEditor : Editor
         _upscalerObject = (Upscaler)serializedObject.targetObject;
         var status = _upscalerObject.Status;
         
-        if (status <= Plugin.UpscalerStatus.NoUpscalerSet)
+        if (Plugin.Success(status))
         {
             _style.normal.textColor = Color.green;
-            _message = "Upscaling successfully running";
+            _message = "Upscaling successfully running.";
         }
         else
         {
             _style.normal.textColor = Color.red;
-            _message = "Upscaler had an Error. Fell back to None.";
+            _message = "Upscaling encountered an Error. Fell back to the 'None' Upscaler.";
         }
         
-        EditorGUILayout.LabelField(new GUIContent("Upscaler Status"), new GUIContent(_message));
+        EditorGUILayout.LabelField(new GUIContent("Upscaler Status",
+            "Provides a description of the current status of the upscaler in plain English."
+            ), new GUIContent(_message));
         
-        EditorGUILayout.LabelField(new GUIContent("Status Code"),
+        EditorGUILayout.LabelField(new GUIContent("Status Code",
+                "Indicates the current 'UpscalerStatus' enum value.\n" +
+                "\nThis can also be accessed via the API using the 'Upscaler.Status' property.\n" +
+                "\nNote: Both 'NoUpscalerSet' and 'Success' indicate that the plugin is working as expected."
+                ),
             new GUIContent(status.ToString()), _style);
+
 
         _basicSettingsFoldout = EditorGUILayout.Foldout(_basicSettingsFoldout, "Basic Upscaler Settings");
         if (_basicSettingsFoldout)
         {
             EditorGUI.indentLevel += 1;
-            EditorGUILayout.PropertyField(_upscalerType, new GUIContent("Upscaler mode"));
-            EditorGUILayout.PropertyField(_qualityMode, new GUIContent("Quality mode"));
+            EditorGUILayout.PropertyField(_upscalerType, new GUIContent("Upscaler",
+                "Choose an Upscaler to use.\n" +
+                "\nUse None to completely disable upscaling.\n" +
+                "\nUse DLSS to enable DLSS upscaling."));
+            EditorGUILayout.PropertyField(_qualityMode, new GUIContent("Quality",
+                "Choose a Quality Mode for the upscaler.\n" +
+                "\nUse Auto to automatically select a Quality Mode based on output resolution:\n" +
+                "< 1920 x 1080 -> Disabled\n" +
+                "<= 2560 x 1440 -> Quality\n" +
+                "<= 3840 x 2160 -> Performance\n" +
+                "> 3840 x 2160 -> Ultra Performance\n" +
+                "\nUse Quality to upscale by 33.3% on each axis.\n" +
+                "\nUse Balanced to upscale by 42% on each axis.\n" +
+                "\nUse Performance to upscale by 50% on each axis.\n" +
+                "\nUse Ultra Performance to upscale by 66.6% on each axis.\n"));
             EditorGUI.indentLevel -= 1;
         }
 
@@ -57,7 +77,10 @@ public class UpscalerEditor : Editor
         if (_advancedSettingsFoldout)
         {
             EditorGUI.indentLevel += 1;
-            EditorGUILayout.Slider(_sharpnessVal, 0, 1, new GUIContent("Sharpness (Deprecated)"));
+            EditorGUILayout.Slider(_sharpnessVal, 0, 1, new GUIContent("Sharpness (Deprecated)",
+                "The amount of sharpening that DLSS should apply to the image.\n" +
+                "\nNote: This only works if DLSS is the the active Upscaler.\n" +
+                "\nNote: This feature is deprecated. NVIDIA suggests shipping your own sharpening solution."));
             EditorGUI.indentLevel -= 1;
         }
 
